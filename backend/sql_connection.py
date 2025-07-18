85cede8ecbc9db4e30431261cc0142425650de6e
manasapatgar22 <mohanpatgar120@gmail.com>
2025-07-18 22:13:01 +0530

    Initial commit

---
+++ backend/sql_connection.py
@@ -0,0 +1,14 @@
+import datetime
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='@Root123', database='storegrocery')
+
+  return __cnx
+
